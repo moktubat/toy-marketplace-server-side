1492e9e810f1e886d71a94483d0584607d0e6da5
moktubat <shadhinreja@gmail.com>
2023-05-18 22:26:29 +0600

    legos json data added

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const legoCollection = client.db('legosDB').collection('legos');
+
+    app.get('/legos', async(req, res) => {
+        const cursor = legoCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

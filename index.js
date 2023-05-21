f4479b02a82a4acfbc0153eb5839e1d678675a20
moktubat <shadhinreja@gmail.com>
2023-05-21 21:16:54 +0600

    vercel data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const AllLegoCollection = client.db("legoDB").collection("allData");
 
@@ -28,6 +28,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/singleData/:id', async(req,res)=>{
+    
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await AllLegoCollection.findOne(query)
+      res.send(result)
+     })
 
     app.get("/marvel", async (req, res) => {
       const query = { sub_category: "Marvel" };

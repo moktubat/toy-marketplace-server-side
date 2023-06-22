c53e8da14fb9c093acc316e8b82d15ceb6589720
moktubat <shadhinreja@gmail.com>
2023-06-22 18:21:52 +0600

    server side fetch problem

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.odqhq4i.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,64 +23,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
-    const AllLegoCollection = client.db("legoDB").collection("allData");
-
-    app.get("/allData", async (req, res) => {
-      const cursor = AllLegoCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    app.get('/singleData/:id', async(req,res)=>{
-    
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await AllLegoCollection.findOne(query)
-      res.send(result)
-     })
-
-    app.get("/marvel", async (req, res) => {
-      const query = { sub_category: "Marvel" };
-      const options = {
-        projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
-      };
-      const cursor = AllLegoCollection.find(query, options).limit(2);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    
-    app.get("/starWars", async (req, res) => {
-      const query = { sub_category: "StarWars" };
-      const options = {
-        projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
-      };
-      const cursor = AllLegoCollection.find(query, options).limit(2);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.get("/DcComics", async (req, res) => {
-      const query = { sub_category: "DcComics" };
-      const options = {
-        projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
-      };
-      const cursor = AllLegoCollection.find(query, options).limit(2);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.get("/transformers", async (req, res) => {
-      const query = { sub_category: "Transformers" };
-      const options = {
-        projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
-      };
-      const cursor = AllLegoCollection.find(query, options).limit(2);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    client.connect();
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -87,11 +37,61 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("server is running");
+});
+
+const AllLegoCollection = client.db("legoDB").collection("allData");
+
+app.get("/allData", async (req, res) => {
+  const cursor = AllLegoCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+app.get("/singleData/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await AllLegoCollection.findOne(query);
+  res.send(result);
+});
+
+app.get("/marvel", async (req, res) => {
+  const query = { sub_category: "Marvel" };
+  const options = {
+    projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
+  };
+  const cursor = AllLegoCollection.find(query, options).limit(2);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+app.get("/starWars", async (req, res) => {
+  const query = { sub_category: "StarWars" };
+  const options = {
+    projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
+  };
+  const cursor = AllLegoCollection.find(query, options).limit(2);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+app.get("/DcComics", async (req, res) => {
+  const query = { sub_category: "DcComics" };
+  const options = {
+    projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
+  };
+  const cursor = AllLegoCollection.find(query, options).limit(2);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+app.get("/transformers", async (req, res) => {
+  const query = { sub_category: "Transformers" };
+  const options = {
+    projection: { _id: 1, image: 1, toy_name: 1, price: 1, rating: 1 },
+  };
+  const cursor = AllLegoCollection.find(query, options).limit(2);
+  const result = await cursor.toArray();
+  res.send(result);
 });
 
 app.listen(port, () => {
